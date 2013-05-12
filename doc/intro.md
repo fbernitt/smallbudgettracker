ca1167bfdd089c8fbb6722802818f8e060ffd41a
Folker Bernitt <folker-bernitt-github@gmx.de>
2013-05-12 18:19:12 +0200

    Initial commit.

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to smallbudgettracker
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/smallbudgettracker/core_test.clj
@@ -0,0 +1,7 @@
+(ns smallbudgettracker.core-test
+  (:require [clojure.test :refer :all]
+            [smallbudgettracker.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
